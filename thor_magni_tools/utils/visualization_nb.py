2eb7500888d5ed04f9d0f4629c0dfe50554c8e7e
tmralmeida <tmr.almeida96@gmail.com>
2023-11-03 14:19:14 +0100

    min social distance per frame_id instead of time

--- thor_magni_tools/utils/visualization_nb.py
+++ thor_magni_tools/utils/visualization_nb.py
@@ -8,5 +8,5 @@ def visualize_scenario(trajectories_df: pd.DataFrame, roles: Optional[dict] = No
     for agent_id in trajectories_df.ag_id.unique():
         traj_agent = trajectories_df[trajectories_df["ag_id"] == agent_id]
         label = agent_id + f" ({roles[agent_id]})" if roles else ""
-        plt.plot(traj_agent["x"], traj_agent["y"], label)
+        plt.plot(traj_agent["x"], traj_agent["y"], label=label)
     plt.legend()
